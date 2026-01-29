33758e4e841c085980d943c5a841cbdae027d6c4
joe <bruhthisisgettingold09@gmail.com>
2026-01-28 19:05:30 -0700

    tvyuvtuiy

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Webhook } = require('discord-webhook-node')
+const hook = new Webhook("https://discord.com/api/webhooks/1466218474094329953/frL3wiiuuleDQtIzniCJc1TF3coY-lgAam2vicp9rRrmcyXKzjkKUiVqQkK32WEilo2U")
 
+hook.send("hi")
 app.get('/', (req, res) => {
 	res.send('Hello World!')
+})
+
+app.post('/', (request, response) => {
+	response.send('webhook request made')
+	console.log(`POST request recieved!`)
+	console.log(JSON.stringify(request))
+
 })
 
 app.listen(port, () => {
