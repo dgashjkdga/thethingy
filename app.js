93309d607d2a845f562a8360e645938c1089d989
joe <bruhthisisgettingold09@gmail.com>
2026-01-28 20:53:39 -0700

    the thing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const BodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const { Webhook } = require('discord-webhook-node')
+app.use(BodyParser.json())
+const { Webhook, MessageBuilder } = require('discord-webhook-node')
 const hook = new Webhook("https://discord.com/api/webhooks/1466218474094329953/frL3wiiuuleDQtIzniCJc1TF3coY-lgAam2vicp9rRrmcyXKzjkKUiVqQkK32WEilo2U")
 
-hook.send("hi")
+hook.send("Initialized")
 app.get('/', (req, res) => {
 	res.send('Hello World!')
 })
@@ -12,8 +14,15 @@ app.get('/', (req, res) => {
 app.post('/', (request, response) => {
 	response.send('webhook request made')
 	console.log(`POST request recieved!`)
-	console.log(JSON.stringify(request))
-
+	webhookmsg = new MessageBuilder()
+	.setTitle('Oracle Incorporated')
+	.addField('Game Name: '+'```'+request.body.gamename+'```', "")
+	.addField('Game Owner: '+'```'+request.body.creatorname+'```', "")
+	.addField('Game ID: '+'```'+request.body.gameid+'```', "")
+	.addField('Server Player Count: '+'```'+request.body.serverplayercount+'```', "")
+	.addField('Javascript Join Code: ','```'+request.body.javascriptjoin+'```')
+	.setColor('#c74634')
+	hook.send(webhookmsg)
 })
 
 app.listen(port, () => {
